8a20ae5d007cd31fcb051c9de14c1e6776f634ed
Chase Skorczewski <chaseskorey@icloud.com>
2022-04-26 15:32:49 -0400

    hour blocks dynamically generate

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,5 +1,29 @@
-let $currentDay = $('#currentDay')
+let $currentDay = $('#currentDay');
+let $container = $('.container');
+let hourCounter = 9;
 
+const generateSchedule = () => {
+
+    while(hourCounter < 18) {
+        let $row = $('<div>').addClass('row time-block');
+        let $time = $('<div>').addClass('col-2 hour');
+        let $task = $('<div>').addClass('col-8 description');
+        let $save = $('<button>').addClass('col-2 saveBtn').html('<i class="fas fa-save"></i>');
+    
+        $time
+        .text(moment({hour:hourCounter}).format('hA'));
+
+        $row.append($time, $task, $save);
+        $container.append($row);
+
+        hourCounter++;
+    }
+}
+
+const checkSchedule = () => {
+    
+}
 
 
 $currentDay.text(moment().format('dddd, MMMM Do'));
+generateSchedule();
