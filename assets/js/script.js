15b20d6bf0ebde4920cfd8e38b7d3253a4ff52a9
Chase Skorczewski <chaseskorey@icloud.com>
2022-04-27 15:49:08 -0400

    now sets and gets local on startup

--- assets/js/script.js
+++ assets/js/script.js
@@ -4,34 +4,45 @@ let hourCounter = moment().hour(9);
 
 let currentTime = moment().format('H');
 
+let tasks = []
+
 $currentDay.text(moment().format('dddd, MMMM Do'));
 
 
+if (!localStorage.getItem('schedule')) {
+    localStorage.setItem('schedule', JSON.stringify(tasks));
+};
 
 // Generates Initial Blocks in Container
 const generateSchedule = () => {
 
     for (let i = 0; i < 9; i++) {
+
+        let milTime = hourCounter.format('H');
+        let standTime = hourCounter.format('hA');
+    
         let $row = $('<form>')
             .addClass('row time-block')
-            .attr('id', `${hourCounter.format('H')}`);
+            .attr('id', `${milTime}`);
 
         let $time = $('<div>')
             .addClass('col-2 hour')
-            .text(hourCounter.format('hA'));
+            .text(standTime);
 
     // add id of mil-time to each block
         let $task = $('<div>')
             .addClass('col-8 description')
-            .attr('id', `${hourCounter.format('H')}`);
+            .attr('id', `${milTime}`)
+            .text(populateFields(milTime, i));
 
         let $save = $('<button>')
             .addClass('col-2 saveBtn')
-            .attr('id', `${hourCounter.format('H')}`)
+            .attr('id', `${milTime}`)
             .html('<i class="fas fa-save"></i>');
     
         $row.append($time, $task, $save);
         $container.append($row);
+
 
         hourCounter.add(1, 'h');
     }
@@ -87,6 +98,27 @@ const addEvents = () => {
         checkHour();
     })
 }
+
+const populateFields = (hour, index) => {
+    let taskStore = localStorage.getItem('schedule');
+    let taskArray = JSON.parse(taskStore)
+
+    if (!taskArray[index]) {
+        let taskObj = {
+            hour: hour,
+            value: ''
+        }
+
+        taskArray.push(taskObj)
+        localStorage.setItem('schedule', JSON.stringify(taskArray))
+
+    } else {
+        
+        return taskArray[index].value
+
+    }
+}
+
 
 generateSchedule();
 setInterval(checkHour, (30*60*1000));
