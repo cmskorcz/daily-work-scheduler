d4c67e7d6b8f47403d99139ea6f37f69ed0035ce
cmskorcz <chaseskorey@icloud.com>
2022-04-26 23:00:11 -0400

    blocks change color according to time

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,29 +1,56 @@
 let $currentDay = $('#currentDay');
 let $container = $('.container');
-let hourCounter = 9;
+let hourCounter = moment().hour(9);
 
+let currentTime = moment().format('H');
+
+$currentDay.text(moment().format('dddd, MMMM Do'));
+
+
+
+// Generates Initial Blocks in Container
 const generateSchedule = () => {
 
-    while(hourCounter < 18) {
-        let $row = $('<div>').addClass('row time-block');
-        let $time = $('<div>').addClass('col-2 hour');
-        let $task = $('<div>').addClass('col-8 description');
-        let $save = $('<button>').addClass('col-2 saveBtn').html('<i class="fas fa-save"></i>');
-    
-        $time
-        .text(moment({hour:hourCounter}).format('hA'));
+    for (let i = 0; i < 9; i++) {
+        let $row = $('<div>')
+            .addClass('row time-block');
 
+        let $time = $('<div>')
+            .addClass('col-2 hour')
+            .text(hourCounter.format('hA'));
+
+    // add id of mil-time to each block
+        let $task = $('<div>')
+            .addClass('col-8 description')
+            .attr('id', `${hourCounter.format('H')}`);
+
+        let $save = $('<button>')
+            .addClass('col-2 saveBtn')
+            .html('<i class="fas fa-save"></i>');
+    
         $row.append($time, $task, $save);
         $container.append($row);
 
-        hourCounter++;
+        hourCounter.add(1, 'h');
     }
+
+    checkHour();
 }
 
-const checkSchedule = () => {
-    
+// Check Schedule Hour if past, present, future 
+const checkHour = () => {
+    $('.description').each(function() {
+        if (parseInt($(this).attr('id')) < parseInt(currentTime)) {
+            $(this).addClass('past')
+        } else if (parseInt($(this).attr('id')) === parseInt(currentTime)) {
+            $(this).addClass('present')
+        } else {
+            $(this).addClass('future')
+        }
+    });
+
+    console.log('checked time')
 }
 
-
-$currentDay.text(moment().format('dddd, MMMM Do'));
 generateSchedule();
+setInterval(checkHour, (30*60*1000));
